38751912b97fc20c4c2539617d565646f2009092
Deva8998 <98423902+Deva8998@users.noreply.github.com>
2023-05-23 12:42:13 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,16 @@
+a=[12,34,56,67,89,87,09]
+console.log(a[5])
+a[2]=45// legal ,works
+a[90]=890
+
+console.log(a)
+console.log(a[87])
+
+
+
+a[-23]=985
+a['place']="chennai"
+b={sno:1,name:"city"}
+a[b]="contact to admine"
+console.log(a.length,"length of array")
+console.log(a)
