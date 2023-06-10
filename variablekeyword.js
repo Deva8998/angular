65d840b93d63934a290708be520b10b3408cbe23
Deva8998 <98423902+Deva8998@users.noreply.github.com>
2023-06-10 18:20:36 +0530

    Delete variablekeyword.js

--- variablekeyword.js
+++
@@ -1,9 +0,0 @@
-//global,function scope and local scope
-a=20
-function printer(){
-var a=25 //function scope variable(if we use var to declare variable then we can scope this variable within that function)
-console.log("a is:",a)
-a=30
-}
-printer()
-console.log("Global a is:",a)
